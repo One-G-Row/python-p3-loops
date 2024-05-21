8f0db8f27260dda1f367e2dfcb11575419751ca8
student <kmac.kimaru93@gmail.com>
2024-05-21 20:59:30 +0300

    adding control flow: loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,39 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    count = 1
+
+    while i > 0:
+        print(i)
+        i -= count
+    
+    print("Happy New Year!")
+
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    double_list = []
+    for int in int_list:
+        double = int ** 2
+        double_list.append(double)
+    return double_list
+
+square_int = square_integers([1, 2, 3, 4, 5])
+print(square_int)
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        
+fizzbuzz()
+        
